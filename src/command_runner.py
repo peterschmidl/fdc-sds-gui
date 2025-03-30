c691a83be7d1442e5972e9959198462675c42e21
Peter Schmidl <peter.schmidl@proton.me>
2025-03-30 17:29:41 +0200

    first version of command runner

--- src/command_runner.py
+++ src/command_runner.py
@@ -1,3 +1,13 @@
+import subprocess
+
 class CommandRunner:
-    def execute():
-        return # TODO
+    def __init__(self):
+        self.sds_command = "fdcsds"
+
+    def execute(self, com_port, baud_rate, disk_paths):
+        subprocess.run(f"{self.sds_command} -p {com_port} -b {baud_rate}") # TODO add disk path(s)
+    
+    def check_fdcsds_present(self):
+        from shutil import which
+        return which(self.sds_command) is not None
+        
