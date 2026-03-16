67e5bc5563e805141e13245d0dcff8d741b53deb
Peter Schmidl <peter.schmidl@proton.me>
2026-03-16 19:48:34 +0100

    get_serial_ports() refined

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,11 @@
+import sys
 import serial.tools.list_ports
 
 class Helpers:
     def get_serial_ports():
         ports = serial.tools.list_ports.comports()
-        return [port.device for port in ports]
+        return [
+            port.device for port in ports
+            if port.serial_number is not None
+            and (sys.platform != 'darwin' or '/cu.' in port.device)
+        ]
