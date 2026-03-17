0b1b93e592d1883808c559107d380a69cfdb7e83
Peter Schmidl <peter.schmidl@proton.me>
2026-03-17 12:03:19 +0100

    changed back to port.name in get_serial_port

--- src/helpers.py
+++ src/helpers.py
@@ -6,7 +6,7 @@ class Helpers:
     def get_serial_ports():
         ports = serial.tools.list_ports.comports()
         return [
-            port.device for port in ports
+            port.name for port in ports
             if port.serial_number is not None
             and (sys.platform != 'darwin' or '/cu.' in port.device)
         ]
