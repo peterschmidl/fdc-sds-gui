fe9b53aec9d333d753e9119547dae576c12526b9
Peter Schmidl <peter.schmidl@proton.me>
2026-03-18 21:17:35 +0100

    option for local development of fdc-sds binary added

--- src/app.py
+++ src/app.py
@@ -11,11 +11,15 @@ if not sys.platform.startswith('darwin') and not sys.platform.startswith('linux'
     Helpers.show_message('Unsupported platform (Linux and MacOS are supported)', QMessageBox.Critical, 'FDC-SDS-GUI Error')
     sys.exit(1)
 
-# Use bundled fdcsds if running as PyInstaller binary, otherwise fall back to PATH
+# Use bundled fdcsds if running as PyInstaller binary, otherwise fall back to submodule or PATH
+_submodule_binary = Path(__file__).resolve().parents[1] / "external/fdc-sds/fdcsds"
+
 if getattr(sys, 'frozen', False):
     fdcsds_path = str(Path(sys._MEIPASS) / 'fdcsds')
     os.chmod(fdcsds_path, 0o755)
     os.environ['FDCSDS_PATH'] = fdcsds_path
+elif _submodule_binary.is_file():
+    os.environ['FDCSDS_PATH'] = str(_submodule_binary)
 elif shutil.which("fdcsds"):
     os.environ['FDCSDS_PATH'] = shutil.which("fdcsds")
 else:
